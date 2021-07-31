a76e41a6b0b33a4e543fd044bff182b856b89804
Jefferson <jefferson_uk@hotmail.com>
2021-07-31 15:28:17 +0200

    configure miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { Summary } from "./components/Summary";
 import { GlobalStyle } from "./styles/global";
 
 export default function App() {
